e6853b6e11e7871bf469bd7a1d7d024112c921c4
dhelennon <d.lennon@f5.com>
2025-11-26 15:12:52 +0000

    adding file to correct location

--- Formula/atomic-commit.rb
+++ Formula/atomic-commit.rb
@@ -1,7 +1,9 @@
+require "language/python/virtualenv"
+
 class AtomicCommit < Formula
   desc "Track uncommitted changes in git repositories with visual indicators"
   homepage "https://github.com/DanLennon/atomic-commit"
-  url "https://github.com/DanLennon/atomic-commit/archive/v1.0.0.tar. gz"
+  url "https://github.com/DanLennon/atomic-commit/archive/v1.0.0.tar.gz"
   sha256 "80d07501130ed434d5eea4e712d6c9b009fe5deb10955b804d54f304f47d6f5c"
   license "MIT"
 
@@ -26,4 +28,3 @@ class AtomicCommit < Formula
     EOS
   end
 end
-EOF
